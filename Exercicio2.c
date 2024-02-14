7f5f8fe633aa62305baecb924f08ad262edb3935
Zythee3 <zmatheusguilherme2@gmail.com>
2024-02-14 12:37:04 -0300

    update

--- Exercicio2.c
+++ Exercicio2.c
@@ -26,7 +26,14 @@ Fila *cria_fila()
 void insere_fila(Fila *fila, int num)
 {
     Elemento *novo = malloc(sizeof(Elemento));
-    if (fila->inicio = NULL)
+    novo->valor = num;
+
+    if (novo == NULL) // Verifica se a alocação de memória falhou
+    {
+        printf("Erro na alocação de memória\n");
+        return;
+    }
+    if (fila->inicio == NULL)
     {
         fila->inicio = novo;
         fila->fim = novo;
@@ -37,15 +44,21 @@ void insere_fila(Fila *fila, int num)
         fila->fim = novo;
     }
 }
-void contador(Fila *fila)
-{   
+
+int contador(Fila *fila)
+{
     int contador = 0;
-    Elemento *leitor = fila->inicio;
-    while(leitor!= NULL){
-        contador++;
+    Elemento *leitor;
+    leitor = fila->inicio;
+
+    while (leitor != NULL)
+    {
+        contador = contador + 1;
         leitor = leitor->proximo;
+        printf("AAAAAA");
     }
     printf("%d", contador);
+    return contador;
 }
 
 int main()
@@ -54,6 +67,8 @@ int main()
     Fila *fila_P1 = cria_fila();
     Fila *fila_P2 = cria_fila();
 
+    int tamanho_P1, tamanho_P2;
+    int opcao_escolha;
     int quantidade_P1 = rand() % 10;
     int quantidade_P2 = rand() % 10;
 
@@ -68,5 +83,26 @@ int main()
         insere_fila(fila_P2, num_P2);
     }
     printf("\n1 - Comparar as filas\n2 - Exibir maior/menor/media\n3 - Transferir elementos\n4 - Exibir numeros impares\n5 - Exibir numeros pares\n");
-    
+    scanf("%d", &opcao_escolha);
+
+    switch (opcao_escolha)
+    {
+    case 1:
+        printf("Sim");
+        tamanho_P1 = contador(fila_P1);
+        // -
+
+        if (tamanho_P1 > tamanho_P2)
+        {
+            printf("\nA fila P1 eh maior que a fila P2 por %d elementos\n", tamanho_P1 - tamanho_P2);
+        }
+        else if (tamanho_P2 > tamanho_P1)
+        {
+            printf("A fila P1 eh maior que a fila P2 por %d elementos\n", tamanho_P2 - tamanho_P1);
+        }
+        break;
+
+    default:
+        break;
+    }
 }
