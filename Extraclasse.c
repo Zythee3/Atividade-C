34cfb5e5e0c1322f0fc877024a727515e0ce38b2
Zythee3 <zmatheusguilherme2@gmail.com>
2024-02-25 16:50:43 -0300

    finalizado

--- Extraclasse.c
+++ Extraclasse.c
@@ -32,13 +32,13 @@ void Imprimir(Fila *fila) {
         printf("--------- cliente %d ---------\nIdade: %d\nPrioridade: %d\n\n",contador, aux->idade, aux->prioridade);
         aux = aux->prox;
     }
-    
+    printf("A lista possui %d idades", contador);
     }
 
 void insere_fila(Fila *fila, int idade, int prioridade) {
-    
     Elemento *novo = malloc(sizeof(Elemento));
-    Elemento *aux, *ant = fila->inicio;
+    Elemento *aux = fila->inicio;
+    Elemento *ant = NULL;
 
     novo->prioridade = prioridade;
     novo->idade = idade;
@@ -47,66 +47,67 @@ void insere_fila(Fila *fila, int idade, int prioridade) {
     if (fila->inicio == NULL) {
         fila->inicio = novo;
         fila->fim = novo;
-        printf("\nPessoa com idade %d de prioridade: %d inserida na fila!\n", novo->idade, novo->prioridade);
-    }   
-    
-    //verifica caso a prioridade do novo numero já seja maior do que o inicio da fila
-    else if(fila->inicio->prioridade < novo->prioridade){
+    } else if (fila->inicio->prioridade < novo->prioridade) {
         novo->prox = fila->inicio;
         fila->inicio = novo;
-        printf("\nPessoa com idade %d de prioridade: %d inserida na fila!\n", novo->idade, novo->prioridade);
-    }
-
-    else {
-        while(aux != NULL){
-            if(aux->prioridade < novo->prioridade){
-                novo->prox = aux;
-                ant->prox = novo;
-                break;
-            }
-            // caso a prioridade da nova idade seja menor que todas as outras idades ela ira ser adicionada no fim da fila
-            if(aux->prox == NULL && aux->prioridade > novo->prioridade){
-                aux->prox = novo;
-                fila->fim = novo;
-                break;
-            }
-            if(aux->idade != ant->idade){
-                ant = ant->prox;
-            }
+    } else {
+        while (aux != NULL && aux->prioridade >= novo->prioridade) {
+            ant = aux;
             aux = aux->prox;
-            
         }
-        printf("\nPessoa com idade %d de prioridade: %d inserida na fila!\n", novo->idade, novo->prioridade);
+        novo->prox = aux;
+        if (ant != NULL) {
+            ant->prox = novo;
+        }
+        if (aux == NULL) {
+            fila->fim = novo;
+        }
     }
+    printf("\nPessoa com idade %d de prioridade: %d inserida na fila!\n", novo->idade, novo->prioridade);
+}
     
+void compara_prioridades(Fila *fila, int prioridade){
+    Elemento *aux = fila->inicio;
+    int contador = 0;
+    
+    while(aux != NULL){
+        if(aux->prioridade == prioridade)
+            contador++;
+        aux = aux->prox;
+    }
+    printf("Quantidade de idades nessa prioridade eh %d\n", contador);
 }
 
 void altera_prioridade(Fila *fila){
     Elemento *aux = fila->inicio;
+    Fila *fila_auxiliar = CriarFila();
     int nova_prioridade;
 
     while(aux != NULL){
-        
-        printf("Insira a nova prioridade da idade %d", aux->idade);
+        printf("Insira a nova prioridade da idade %d: ", aux->idade);
         scanf("%d", &nova_prioridade);
         printf("prioridade alterada");
-        insere_fila(fila, aux->idade, nova_prioridade);
+        fila->inicio = fila->inicio->prox;
+        insere_fila(fila_auxiliar, aux->idade, nova_prioridade);
         free(aux);
-        aux= aux->prox;
+        aux = fila->inicio;
 
     }
+    aux = fila_auxiliar->inicio;
+    while(aux != NULL){
+        insere_fila(fila, aux->idade, aux->prioridade);
+        aux = aux->prox;
+    }
+    
 }
-
 
 
 int main(){
     Fila *fila_primaria = CriarFila();
-    Fila *fila_auxiliar = CriarFila();
-
-    int menu, idade, prioridade;
-    while(menu != 4){
+    int menu, idade, prioridade, case3;
+    while(menu != 5){
         
-        printf("\n1 - Insere idade\n2 - Exibir quantidade de numeros na fila\n3 - Comparar prioridades\n4 - Sair\n");
+        printf("\n1 - Insere idade\n2 - Altera a prioridade\n3 - Exibir quantidade de numeros na fila\n4 - Comparar prioridades\n5 - Sair\n");
         scanf("%d", &menu);
         switch (menu)
         {
@@ -118,7 +119,15 @@ int main(){
             insere_fila(fila_primaria, idade, prioridade);
             break;
         case 2:
+            altera_prioridade(fila_primaria);
+            break;
+        case 3:
             Imprimir(fila_primaria);
+            break;
+        case 4:
+            printf("Digite a prioridade que seja analisar: ");
+            scanf("%d", &case3);
+            compara_prioridades(fila_primaria, case3);
             break;
         default:
             break;
